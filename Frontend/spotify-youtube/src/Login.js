7ff134313faa1fb22e612ccf2671db1b78244fd0
riyadhar8 <f20170753@goa.bits-pilani.ac.in>
2021-07-07 21:01:46 +0530

    Add login page

---
+++ Frontend/spotify-youtube/src/Login.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from "react";
+import { Button } from 'reactstrap';
+
+const axios = require('axios');
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state=({
+      link: {}
+    })
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin() {
+    var self = this;
+    axios
+    .get('http://localhost:5000/login')
+    .then(function (response) {
+      console.log(response.data);
+      self.setState({link: response.data})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="logo" style={{ height: 424, width: 288 }}/>
+        </header>
+        <div className="App-body">
+            <Button onClick={this.handleLogin} className="App-link" block rel="noopener noreferrer" target="_blank">
+              Login to Spotify
+            </Button> 
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
