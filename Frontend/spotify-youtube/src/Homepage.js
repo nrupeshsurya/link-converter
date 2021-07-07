9ad012a19ac6be434118ff9ccef0efa2b59c34a4
riyadhar8 <f20170753@goa.bits-pilani.ac.in>
2021-07-07 23:10:16 +0530

    Add redirect to Homepage

--- Frontend/spotify-youtube/src/Homepage.js
+++ Frontend/spotify-youtube/src/Homepage.js
@@ -1,32 +1,57 @@
-import React from 'react';  
+import React, { Component } from "react";  
 import './App.css';  
 import YoutubeToSpotify from './YoutubeToSpotify'
 import SpotifyToYoutube from './SpotifyToYoutube';  
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';  
 
-function Homepage() {  
-  return (   
-    <Router>    
-      <div className="container">    
-        <nav className="navbar navbar-expand-lg navheader">    
-          <div className="collapse navbar-collapse" >    
-            <ul className="navbar-nav">    
-              <li className="nav-item">    
-                <NavLink to={'/YoutubeToSpotify'} activeClassName="active">YouTube to Spotify</NavLink>    
-              </li>    
-              <li className="nav-item">    
-                <NavLink to={'/SpotifyToYouTube'} activeClassName="active">Spotify To YouTube</NavLink>    
-              </li>   
-            </ul>    
-          </div>    
-        </nav> <br />    
-        <Switch>    
-          <Route path='/YoutubeToSpotify' component={YoutubeToSpotify} />    
-          <Route path='/SpotifyToYoutube' component={SpotifyToYoutube} />  
-        </Switch>  
-      </div>    
-    </Router>   
-  );  
-}  
+const axios = require('axios');
+class Homepage extends Component {
+    constructor() {
+        super();
+        this.state=({
+          name: ''
+        })
+        this.getData = this.getData.bind(this);
+        this.getData();
+    }
+
+    getData() {
+        var self = this;
+        axios
+        .get('http://localhost:5000/home')
+        .then(function (response) {
+        console.log(response.data);
+        self.setState({name: response.data})
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+    }
+
+    render() {
+        return (   
+            <Router>    
+              <div className="container">    
+                <nav className="navbar navbar-expand-lg navheader">    
+                  <div className="collapse navbar-collapse" >    
+                    <ul className="navbar-nav">    
+                      <li className="nav-item">    
+                        <NavLink to={'/YoutubeToSpotify'} activeClassName="active">YouTube to Spotify</NavLink>    
+                      </li>    
+                      <li className="nav-item">    
+                        <NavLink to={'/SpotifyToYouTube'} activeClassName="active">Spotify To YouTube</NavLink>    
+                      </li>   
+                    </ul>    
+                  </div>    
+                </nav> <br />    
+                <Switch>    
+                  <Route path='/YoutubeToSpotify' component={YoutubeToSpotify} />    
+                  <Route path='/SpotifyToYoutube' component={SpotifyToYoutube} />  
+                </Switch>  
+              </div>    
+            </Router>   
+        );
+    }
+}
 
 export default Homepage;
